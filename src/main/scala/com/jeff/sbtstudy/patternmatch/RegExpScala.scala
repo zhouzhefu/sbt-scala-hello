54af1eb2adf8632d142ef4f94cf7daf6d1235f63
Jeff Zhou <zhou.zhefu@gmail.com>
2015-03-16 03:10:53 +0800

    mark RegExp feature is not that important in scala lang

--- src/main/scala/com/jeff/sbtstudy/patternmatch/RegExpScala.scala
+++ src/main/scala/com/jeff/sbtstudy/patternmatch/RegExpScala.scala
@@ -2,6 +2,9 @@ package com.jeff.sbtstudy.patternmatch
 
 /**
  * Created by winniewang on 15/3/15.
+ *
+ * Right-ignoring pattern, it was retracted by Scala for a period but it seems working again in v2.11
+ * Anyway, it is not a very useful feature.
  */
 object RegExpScala {
   def containScala(s: String): Boolean = {
